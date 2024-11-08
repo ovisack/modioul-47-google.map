e41c721d4a20baf31e3b505f3b4d6da91669bdd7
ovisack <royavishek306@gmail.com>
2024-11-09 00:59:23 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { Map } from '@vis.gl/react-google-maps'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <Map
+      style={{width: '100vw', height: '100vh'}}
+      defaultCenter={{lat: 23.8041, lng: 90.4152}}
+      defaultZoom={3}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+  />
+      
+    </>
+  )
+}
+
+export default App
